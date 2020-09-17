a6eefd3dc445ca1db8d16696f9399d809e437944
Taylor Meyer <taylor.ross.meyer@gmail.com>
2020-09-16 23:07:02 -0700

    Fire event when player opens taxi map

--- MissingFlightPathFinder.lua
+++ MissingFlightPathFinder.lua
@@ -1,1 +1,15 @@
 print("MISSING FP FINDER")
+
+CreateFrame("Frame", "TaxiOpenEventFrame", UIParent)
+
+TaxiOpenEventFrame:RegisterEvent("TAXIMAP_OPENED")
+
+TaxiOpenEventFrame:SetScript("OnEvent", function(self, event, ...)
+
+	if event == "TAXIMAP_OPENED" then
+	
+		print("TAXIMAP_OPENED fired success")
+	
+	end
+
+end)
