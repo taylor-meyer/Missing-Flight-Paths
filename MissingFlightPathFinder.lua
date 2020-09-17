38a02620d2294f71313064e7ac448b062c500d0c
Taylor Meyer <taylor.ross.meyer@gmail.com>
2020-09-16 23:30:44 -0700

    Print unreachable flight path names on taxi map open

--- MissingFlightPathFinder.lua
+++ MissingFlightPathFinder.lua
@@ -9,6 +9,17 @@ TaxiOpenEventFrame:SetScript("OnEvent", function(self, event, ...)
 	if event == "TAXIMAP_OPENED" then
 	
 		print("TAXIMAP_OPENED fired success")
+		taxiNodes = C_TaxiMap.GetAllTaxiNodes(WorldMapFrame:GetMapID())
+		
+		for i=1,table.getn(taxiNodes) do
+		
+			if taxiNodes[i].state == 2 then
+			
+			 print(i .. ": " .. taxiNodes[i].name .. "    Reachable: " .. taxiNodes[i].state)
+			
+			end
+	
+		end
 	
 	end
 
