da2e8e460c681efc0be984e48c8ae460e5ba61c7
Taylor Meyer <taylor.ross.meyer@gmail.com>
2020-11-07 11:37:26 -0800

    Create table of Tiragard Sound ferry nodes

--- IgnoredNodes.lua
+++ IgnoredNodes.lua
@@ -4,3 +4,51 @@
 -- Author: Lypidius <Axiom> @ US-MoonGuard
 ------------------------------------------------------------------------------------------
 local addon, ns = ... -- Addon name & common namespace
+
+KulTirasFerries = {
+	{
+		name = "Anglepoint Wharf, Tiragarde Sound",
+		x = 0.43265134096146,
+		y = 0.53354406356812
+	},
+	{
+		name = "Old Drust Road, Tiragarde Sound",
+		x = 0.50041884183884,
+		y = 0.6606656908989
+	},
+	{
+		name = "Firebreaker Expedition, Tiragarde Sound",
+		x = 0.55638957023621,
+		y = 0.53236401081085
+	},
+	{
+		name = "Southwind Station, Tiragarde Sound",
+		x = 0.57262217998505,
+		y = 0.64078992605209
+	},
+	{
+		name = "Fallhaven, Drustvar",
+		x = 0.44013005495071,
+		y = 0.63971048593521
+	},
+	{
+		name = "Fletcher's Hollow, Drustvar",
+		x = 0.47386223077774,
+		y = 0.73052299022675
+	},
+	{
+		name = "Eastpoint Station, Tiragarde Sound",
+		x = 0.61797845363617,
+		y = 0.60975468158722
+	},
+	{
+		name = "Eastpoint Station, Tiragarde Sound",
+		x = 0.61410176753998,
+		y = 0.60993653535843
+	},
+	{
+		name = "Tradewinds Market, Tiragarde Sound",
+		x = 0.62087142467499,
+		y = 0.50514781475067
+	}
+}
