bb243d47e0b6c42af2d21d67bc5aaea570afe0b2
Taylor Meyer <taylor.ross.meyer@gmail.com>
2020-11-06 17:47:09 -0800

    Code cleanup

--- MissingFlightPathFinder.lua
+++ MissingFlightPathFinder.lua
@@ -1,21 +1,31 @@
-
+------------------------------------------------------------------------------------------
+-- Missing Flight Paths --
+--------------------
+-- Author: Lypidius <Axiom> @ US-MoonGuard
+------------------------------------------------------------------------------------------
 
 local Marks = {}
 
 local InvalidNames = {
+	-- Unused by both factions
 	"Southwind Village, Silithus",
-	"Schnottz's Landing, Uldum",
+	-- For a quest, not in flight path network
 	"Quest - Hellfire Peninsula (Alliance) End",
+	-- One way from Borean Tundra to Coldarra, not in flight path network
 	"Amber Ledge, Borean (to Coldarra)",
 	"Transitus Shield, Coldarra (NOT USED)",
-	
-	-- These are temporary until I learn what is broken about them
-	"Atal'Gral, Zuldazar",
+	-- Unused by both factions
 	"Dreadpearl, Zuldazar",
-	"Devoted Sanctuary, Vol'dun"
+	-- These are temporarily ignored until I learn what is broken about them
+	"Atal'Gral, Zuldazar",
+	"Devoted Sanctuary, Vol'dun",
+	-- In present time Uldum, this node is unavailable. Temporarily ignored
+	"Schnottz's Landing, Uldum"
 }
 
-local DraenorMapIDs = {
+local TaxiFrameIDs = {
+	870, -- Pandaria
+	1064, -- Isle of Thunder
 	1116, -- Draenor
 	1191, -- Ashran
 	1464, -- Tanaan Jungle
@@ -25,11 +35,6 @@ local DraenorMapIDs = {
 	1158, -- Alliance Garrison lv1
 	1331, -- Alliance Garrison lv2
 	1159  -- Alliance Garrison lv3
-}
-
-local PandariaMapIDs = {
-	870, -- Pandaria
-	1064 -- Isle of Thunder
 }
 
 -- For Vashj'ir
@@ -44,74 +49,49 @@ local SeahorseIDs = {
 local UnderwaterNodesNonDK = {22,23,24,25,30,32}
 local UnderwaterNodesDK = {23,24,25,26,31,33}
 
-
+-- Event frame
 CreateFrame("Frame", "TaxiOpenEventFrame", UIParent)
-
 TaxiOpenEventFrame:RegisterEvent("TAXIMAP_OPENED")
 TaxiOpenEventFrame:RegisterEvent("TAXIMAP_CLOSED")
-
 TaxiOpenEventFrame:SetScript("OnEvent", function(self, event, ...)
 	if event == "TAXIMAP_OPENED" then
 		ClearAllMarks()
 		
-		--PrintAllNodesAlt()
-		
 		-- Vashj'ir
 		local _, _, _, _, _, _, _, instanceID, _, _ = GetInstanceInfo()
 		if instanceID == 0 then -- Eastern Kingdoms
-		
-			
 			if AtUnderwaterNode() == true then -- Show underwater nodes but not rest of continent
 				PlaceVashjirUnderwaterNodes()
 			else -- Show EK nodes but not the underwater ones
 				PlaceEasternKingdomsNodes()
 			end
-		
-		
-		elseif PlayerInDraenor() == true or PlayerInPandaria() == true then
-			PlaceDraenorPoints()
-		
-		
+		elseif DraenorOrPandaria() == true then
+			PlaceNodes(true)
 		else
-			for i=1,NumTaxiNodes() do
-				local x,y = TaxiNodePosition(i)
-				local Type = TaxiNodeGetType(i)
-				local name = TaxiNodeName(i)
-				
-				--PrintNodeInfo(i, name, Type, x, y)
-				
-				
-				--PrintNodeInfo(i, name, Type, x, y)
-
-				--[=[
-				if name == "Devoted Sanctuary, Vol'dun" then
-					PrintNodeInfo(i, name, Type, x, y)
-				end
-				]=]
-				
-				if Type == "REACHABLE" and ValidFP(name) == true then
-					--PrintNodeInfo(i, name, Type, x, y)
-				end
-					
-				if Type == "DISTANT" and ValidFP(name) == true then
-					--PrintNodeInfo(i, name, Type, x, y)
-					PlacePoint(TaxiNodeName(i), x*100, y*100)
-				end
-			end
+			PlaceNodes(false)
 		end
-		
-		
 	end
 end)
 
-function PlacePoint(name, x, y)
+function PlacePoint(name, x, y, isDraenor)
 
-	local f = FlightMapFrame.ScrollContainer.Child
+	local f = nil
+	local width = nil
+	local height = nil
+	if isDraenor == true then
+		f = TaxiRouteMap
+		width = 16
+		height = 16
+	else
+		f = FlightMapFrame.ScrollContainer.Child
+		width = 75
+		height = 75
+	end
 	
 	
 	local pin = CreateFrame("Frame", "MFPPin_" .. name, f)
-	pin:SetWidth(75)
-	pin:SetHeight(75)
+	pin:SetWidth(width)
+	pin:SetHeight(height)
 	
 	pin:HookScript("OnEnter", function()
 			GameTooltip:SetOwner(pin, "ANCHOR_TOP")
@@ -137,15 +117,8 @@ function PlacePoint(name, x, y)
 end
 
 function ValidFP(name)
-
-	local uiMapID = C_Map.GetBestMapForUnit("player")
-
 	for i=1,table.getn(InvalidNames) do
-	
-		if name == "Schnottz's Landing, Uldum" and uiMapID == 1527 then
-			return false
-	
-		elseif name == InvalidNames[i] then
+		if name == InvalidNames[i] then
 			return false
 		end
 	end
@@ -159,94 +132,26 @@ function ClearAllMarks()
 	Marks = {}
 end
 
-function PrintInfoByIndex(i)
-
-	local x,y = TaxiNodePosition(i)
-	local Type = TaxiNodeGetType(i)
-	local name = TaxiNodeName(i)
-
-	print("i: " .. i)
-	print("name: " .. name)
-	print("type: " .. Type)
-	print("x: " .. x*100 .. " y: " .. y*100)
-	print()
-	
-end
-
-function PrintNodeInfo(i, name, Type, x, y)
-	print("i: " .. i .. " name: " .. name .. " type: " .. Type)
-	--print("x: " .. x*100 .. " y: " .. y*100)
-end
-
-function PlaceDraenorPoints()
+function PlaceNodes(isTaxiFrame)
 	for i=1,NumTaxiNodes() do
 		local x,y = TaxiNodePosition(i)
 		local Type = TaxiNodeGetType(i)
 		local name = TaxiNodeName(i)
 		if Type == "DISTANT" and ValidFP(name) == true then
-			--PrintNodeInfo(i, name, Type, x, y)
-			PlaceDraenorPoint(TaxiNodeName(i), x*100, y*100, true)
+			PlacePoint(TaxiNodeName(i), x*100, y*100, isTaxiFrame)
 		end
 	end
 end
 
-function PlaceDraenorPoint(name, x, y)
-	
-	local f = TaxiRouteMap
-	local pin = CreateFrame("Frame", "MFPPin_" .. name, f)
-	
-	pin:SetWidth(16)
-	pin:SetHeight(16)
-	
-	pin.texture = pin:CreateTexture()
-	pin.texture:SetTexture("Interface\\MINIMAP\\ObjectIcons.blp")
-	pin.texture:SetTexCoord(0.625, 0.750, 0.125, 0.250)
-	pin.texture:SetAllPoints()
-	
-	pin:SetFrameStrata("TOOLTIP")
-	pin:SetFrameLevel(f:GetFrameLevel() + 1)
-	pin:SetPoint("CENTER", f, "TOPLEFT", x / 100 * f:GetWidth(), -y / 100 * f:GetHeight())
-	
-	
-	
-	pin:HookScript("OnEnter", function()
-			GameTooltip:SetOwner(pin, "ANCHOR_TOP")
-			GameTooltip:AddLine(name, 0, 1, 0)
-			GameTooltip:Show()
-	end)
-	
-	pin:HookScript("OnLeave", function()
-		GameTooltip:Hide()
-	end)
-	
-	
-	
-	Marks[table.getn(Marks) + 1] = pin
-	pin:Show()
-end
-
-function PlayerInDraenor()
+function DraenorOrPandaria()
 	local _, _, _, _, _, _, _, instanceID, _, _ = GetInstanceInfo()
-	for i=1,table.getn(DraenorMapIDs) do
-		if DraenorMapIDs[i] == instanceID then
+	for i=1,table.getn(TaxiFrameIDs) do
+		if TaxiFrameIDs[i] == instanceID then
 			return true
 		end
 	end
 	return false
 end
-
-function PlayerInPandaria()
-	local _, _, _, _, _, _, _, instanceID, _, _ = GetInstanceInfo()
-	for i=1,table.getn(DraenorMapIDs) do
-		if PandariaMapIDs[i] == instanceID then
-			return true
-		end
-	end
-	return false
-end
-
-
-
 
 function AtUnderwaterNode()
 	local guid = UnitGUID("target")
@@ -258,110 +163,45 @@ function AtUnderwaterNode()
 	return false
 end
 
-
-
-
-function PrintAllNodes()
-	taxiNodes = C_TaxiMap.GetAllTaxiNodes(WorldMapFrame:GetMapID())
-	for i=1,table.getn(taxiNodes) do
-		print("i: " .. i .. "  nodeID: " .. taxiNodes[i].nodeID .. "  name: " .. taxiNodes[i].name .. "  type: " .. taxiNodes[i].state)
-	end
-end
-
-function PrintAllNodesAlt()
-	print(GetTaxiMapID())
-	for i=1,NumTaxiNodes() do
-		local x,y = TaxiNodePosition(i)
-		local Type = TaxiNodeGetType(i)
-		local name = TaxiNodeName(i)
-		print("i: " .. i .. " name: " .. name .. " type: " .. Type)
-	end
-end
-
-function PrintAllNodesAltDistant()
-	print(GetTaxiMapID())
-	for i=1,NumTaxiNodes() do
-		local x,y = TaxiNodePosition(i)
-		local Type = TaxiNodeGetType(i)
-		local name = TaxiNodeName(i)
-		if Type == "DISTANT" then
-			print("i: " .. i .. " name: " .. name .. " type: " .. Type)
-		end
-	end
-end
-
-function PrintAllNodesAltReachable()
-	for i=1,NumTaxiNodes() do
-		local x,y = TaxiNodePosition(i)
-		local Type = TaxiNodeGetType(i)
-		local name = TaxiNodeName(i)
-		if Type == "REACHABLE" or Type == "CURRENT" then
-			print("i: " .. i .. " name: " .. name .. " type: " .. Type)
-		end
-	end
-end
-
 function PlaceVashjirUnderwaterNodes()
 		local _, classFilename, _ = UnitClass("player")
-		
 		-- Death Knight flight table is different, so we use the other table for underwater nodes
 		if classFilename == "DEATHKNIGHT" then
-			
 			for i=1,table.getn(UnderwaterNodesDK) do
-			
 				local x,y = TaxiNodePosition(UnderwaterNodesDK[i])
 				local Type = TaxiNodeGetType(UnderwaterNodesDK[i])
 				local name = TaxiNodeName(UnderwaterNodesDK[i])
-				
 				if Type == "DISTANT" then
 					PlacePoint(name, x*100, y*100)
 				end
-			
-			
 			end
-			
-			
-			
 		else
-		
 			for i=1,table.getn(UnderwaterNodesNonDK) do
-			
 				local x,y = TaxiNodePosition(UnderwaterNodesNonDK[i])
 				local Type = TaxiNodeGetType(UnderwaterNodesNonDK[i])
 				local name = TaxiNodeName(UnderwaterNodesNonDK[i])
-				
 				if Type == "DISTANT" then
 					PlacePoint(name, x*100, y*100)
 				end
-			
-			
 			end
-		
-		
 		end
 end
 
 function PlaceEasternKingdomsNodes()
 	for i=1,NumTaxiNodes() do
-		
 		if IsUnderwaterIndex(i) == false then
-		
 			local x,y = TaxiNodePosition(i)
 			local Type = TaxiNodeGetType(i)
 			local name = TaxiNodeName(i)
 			if Type == "DISTANT" then
-				--print("i: " .. i .. " name: " .. name .. " type: " .. Type)
 				PlacePoint(name, x*100, y*100)
 			end
-			
 		end
 	end
 end
 
 function IsUnderwaterIndex(index)
-
 	local _, classFilename, _ = UnitClass("player")
-		
 	-- Death Knight flight table is different, so we use the other table for underwater nodes
 	if classFilename == "DEATHKNIGHT" then
 		for i=1,table.getn(UnderwaterNodesDK) do
@@ -380,24 +220,39 @@ function IsUnderwaterIndex(index)
 end
 
 
+-- Printouts for testing
+function PrintAllNodes()
+	print("TaxiMapID: " .. GetTaxiMapID())
+	for i=1,NumTaxiNodes() do
+		local x,y = TaxiNodePosition(i)
+		local Type = TaxiNodeGetType(i)
+		local name = TaxiNodeName(i)
+		print("i: " .. i .. " name: " .. name .. " type: " .. Type)
+	end
+end
+
+function PrintAllNodesAltDistant()
+	print("TaxiMapID: " .. GetTaxiMapID())
+	for i=1,NumTaxiNodes() do
+		local x,y = TaxiNodePosition(i)
+		local Type = TaxiNodeGetType(i)
+		local name = TaxiNodeName(i)
+		if Type == "DISTANT" then
+			print("i: " .. i .. " name: " .. name .. " type: " .. Type)
+		end
+	end
+end
+
+function PrintAllNodesAltReachable()
+	print("TaxiMapID: " .. GetTaxiMapID())
+	for i=1,NumTaxiNodes() do
+		local x,y = TaxiNodePosition(i)
+		local Type = TaxiNodeGetType(i)
+		local name = TaxiNodeName(i)
+		if Type == "REACHABLE" or Type == "CURRENT" then
+			print("i: " .. i .. " name: " .. name .. " type: " .. Type)
+		end
+	end
+end
+
 print("MFP loaded.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
